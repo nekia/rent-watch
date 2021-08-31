393b934b249a1e0a82111c19f47cbfbeaf421b0c
Atsushi Neki <nekiaiken@gmail.com>
2021-09-01 02:44:56 +0900

    Add scan for each room page

--- index.js
+++ index.js
@@ -2,23 +2,52 @@ const playwright = require('playwright');
 const axios = require('axios');
 const qs = require('querystring');
 
-
 const BASE_URL = 'https://notify-api.line.me';
 const PATH = '/api/notify';
 const LINE_TOKEN = process.env.LINE_NOTIFY_TOKEN;
 
 const checkUrl = 'https://www.linea.co.jp/article/roomlist/type/rent/id/304/fl/all';
-const watchLists = [
-  '201',
-  '301',
-  '401',
-  '501',
-  '601',
-  '603',
-  '701',
-  '801',
-  '802'
-];
+const roomUrl = 'https://www.linea.co.jp/article/room/type/rent/id/304/rid'
+const watchLists = {
+  rooms: [
+    {
+      number: '201',
+      id: '2060'
+    },
+    {
+      number: '301',
+      id: '2065'
+    },
+    {
+      number: '401',
+      id: '2070'
+    },
+    {
+      number: '501',
+      id: '2075'
+    },
+    {
+      number: '601',
+      id: '2080'
+    },
+    {
+      number: '603',
+      id: '2082'
+    },
+    {
+      number: '701',
+      id: '2083'
+    },
+    {
+      number: '801',
+      id: '2086'
+    },
+    {
+      number: '802',
+      id: '2086'
+    }
+  ]
+};
 
 const config = {
   baseURL: BASE_URL,
@@ -39,11 +68,11 @@ const config = {
   const page = await context.newPage();
   await page.goto(checkUrl);
   const rooms = await page.$$('.room-number');
-  for ( let room of rooms ) {
+  for (let room of rooms) {
     const roomNumber = await room.innerText();
-    for ( const watch of watchLists ) {
-      if ( roomNumber.includes(watch) ) {
-        console.log(`Available: room ${watch} !!`)
+    for (const watch of watchLists.rooms) {
+      if (roomNumber.includes(watch.number)) {
+        console.log(`Available: room ${watch.number} !!`)
         config.data = qs.stringify({
           message: `${roomNumber}号室が空いてます! ${checkUrl}`,
         })
@@ -53,6 +82,21 @@ const config = {
     }
     console.log(roomNumber)
   }
-  await page.screenshot({ path: `all-lists.png` });
+  // await page.screenshot({ path: `all-lists.png` });
+
+  for (const watch of watchLists.rooms) {
+    const watchUrl = `${roomUrl}/${watch.id}`;
+    await page.goto(watchUrl);
+    const roomSpecs = await page.$$('//ul[@class="room-main-floor-list"]/li')
+    const rent = await roomSpecs[0].innerText();
+    if ( !rent.includes('未定')　) {
+      console.log(`Preparing room [${watch.number}] ${rent} !!`)
+      config.data = qs.stringify({
+        message: `${watch.number}号室が準備中です! ${rent} : ${watchUrl}`,
+      })
+      const response = await axios.request(config);
+  }
+    // await page.screenshot({ path: `room-${watch.number}.png` });
+  }
   await browser.close();
 })();
