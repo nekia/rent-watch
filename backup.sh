fe705b4f240e3f555570d481142c0268ef525176
Atsushi Neki <nekiaiken@gmail.com>
2021-11-23 01:36:34 +0000

    Add BASEDIR to backup script

--- backup.sh
+++ backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir backup 2>/dev/null
+BASEDIR=$(dirname "$0")
+mkdir ${BASEDIR}/backup 2>/dev/null
 ps auxw | grep kube-apiservers | grep -v grep > /dev/null
 if [ $? -eq 0 ]; then
-	docker run --rm -it -v $PWD/backup:/home/redis-backup redis redis-cli -u "redis://192.168.2.132:32565" --rdb /home/redis-backup/backup.rdb
+	docker run --rm -it -v ${BASEDIR}/backup:/home/redis-backup redis redis-cli -u "redis://192.168.2.132:32565" --rdb /home/redis-backup/backup-`date +%Y%m%d`.rdb
 else
 	kubectl exec -i -n rent rent-redis-594bf88844-bfqkk -- redis-cli --rdb /tmp/backup-`date +%Y%m%d`.rdb
-	kubectl cp  rent/rent-redis-594bf88844-bfqkk:/tmp ./backup
+	kubectl cp  rent/rent-redis-594bf88844-bfqkk:/tmp ${BASEDIR}/backup
 fi
