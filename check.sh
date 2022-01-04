e3dfebac5c9ebfe924169ae29bcaf71615dbbacf
Atsushi Neki <nekiaiken@gmail.com>
2022-01-04 15:25:39 +0000

    Update script

--- check.sh
+++ check.sh
@@ -6,6 +6,7 @@ BASEDIR=$(dirname "$0")
 pushd "$BASEDIR"
 export LINE_NOTIFY_TOKEN=YOUR_LINE_NOTIFY_TOKEN_HERE
 #/usr/local/bin/node index.js
+/usr/local/bin/node index-rnet.js
 /usr/local/bin/node index-main.js
 /usr/local/bin/node index-mitsui.js
 /usr/local/bin/node index-linea.js
