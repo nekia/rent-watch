1f6d64d02fe2872d7ac6cd5e2586b26f8a36c145
Atsushi Neki <nekiaiken@gmail.com>
2022-01-11 09:54:09 +0900

    Update redis pod name

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ BASEDIR=$(dirname "$0")
 mkdir ${BASEDIR}/backup 2>/dev/null
 ps auxw | grep kube-apiservers | grep -v grep > /dev/null
 if [ $? -eq 0 ]; then
-	docker run --rm -it -v ${BASEDIR}/backup:/home/redis-backup redis redis-cli -u "redis://192.168.2.132:32565" --rdb /home/redis-backup/backup-`date +%Y%m%d`.rdb
+	docker run --rm -it -v ${BASEDIR}/backup:/home/redis-backup redis redis-cli -u "redis://192.168.2.132:32089" --rdb /home/redis-backup/backup-`date +%Y%m%d`.rdb
 else
-	kubectl exec -i -n rent rent-redis-594bf88844-bfqkk -- redis-cli --rdb /tmp/backup-`date +%Y%m%d`.rdb
-	kubectl cp  rent/rent-redis-594bf88844-bfqkk:/tmp ${BASEDIR}/backup
+	kubectl exec -i -n rent rent-redis-594bf88844-xsfnf -- redis-cli --rdb /tmp/backup-`date +%Y%m%d`.rdb
+	kubectl cp  rent/rent-redis-594bf88844-xsfnf:/tmp ${BASEDIR}/backup
 fi
