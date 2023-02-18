b683f59e6e9a96f84c1157ec4797a7e858760c8e
Atsushi Neki <nekiaiken@gmail.com>
2023-02-18 23:51:02 +0900

    Add R-Store

--- scanner/rstore/index.js
+++ scanner/rstore/index.js
@@ -3,9 +3,8 @@ const playwright = require('playwright-chromium');
 const express = require('express');
 const http = require('http');
 
+const SITE_NAME = "R-STORE";
 const nats_server_url = process.env.NATS_SERVER_URL ? process.env.NATS_SERVER_URL : "127.0.0.1:4222";
-const nats_subject_roomurl = process.env.NATS_SUBJECT_ROOMURL ? process.env.NATS_SUBJECT_ROOMURL : "room-rstore-tky-rent";
-const nats_subject_roomdetail = process.env.NATS_SUBJECT_ROOMDETAIL ? process.env.NATS_SUBJECT_ROOMDETAIL : "roomdetails-tky-rent";
 
 getNewContext = async (browser) => {
   const ctx = await browser.newContext({
@@ -24,7 +23,7 @@ scanRoomDetail = async (address) => {
   const browser = await playwright['chromium'].launch({ headless: true });
   const context = await getNewContext(browser);
   const roomPage = await getNewPage(context);
-  let price = 0.0, size = 0.0, floorLevel = {}, location = "", builtYear = 0;
+  let price = 0.0, size = 0.0, floorLevel = {}, location = "", builtYear = 0, isPetOK = false;
   try {
     await roomPage.goto(address);
     await roomPage.waitForTimeout(1000)
@@ -33,12 +32,13 @@ scanRoomDetail = async (address) => {
     floorLevel = await getFloorLevel(roomPage)
     location = await getLocation(roomPage)
     builtYear = await getBuiltYear(roomPage)
+    isPetOK = await getPetOK(roomPage)
   } catch (error) {
     console.warn('## Failed to retrieve the detail ##', address, error)
   }
   await roomPage.close()
   await browser.close();
-  return { address, price, size, floorLevel, location, builtYear }
+  return { address, price, size, floorLevel, location, builtYear, isPetOK }
 }
 
 getPriceFloat = async (page) => {
@@ -84,48 +84,43 @@ getBuiltYear = async (page) => {
   })
 }
 
+getPetOK = async (page) => {
+  return page.$('//h3[text()="ペット飼育"]/following-sibling::p[1]')
+  .then( p => p.innerText() )
+  .then( str => str != "不可" )
+}
+
 (async () => {
-  const app = express();
-  const router = express.Router();
 
-  router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Methods', 'GET');
-    next();
-  });
-
-  router.get('/health', (req, res) => {
-    const data = {
-      uptime: process.uptime(),
-      message: 'Ok',
-      date: new Date()
-    }
-  
-    res.status(200).send(data);
-  });
-
-  app.use('/api/v1', router);
-
-  const server = http.createServer(app);
-  server.listen(3000);
-
+  // to create a connection to a nats-server:
   const nc = await nats.connect({ servers: nats_server_url });
-  const js = nc.jetstream();
 
   // create a codec
   const sc = nats.StringCodec();
   const jc = nats.JSONCodec();
   // create a simple subscriber and iterate over messages
   // matching the subscription
-  const sub = nc.subscribe(nats_subject_roomurl, { queue: "room" });
-  (async () => {
-    for await (const m of sub) {
-      const address = sc.decode(m.data);
-      console.log(`[${sub.getProcessed()}]: ${address}`);
 
-      const detailObj = await scanRoomDetail(address);
-      console.log(detailObj)
-      js.publish(nats_subject_roomdetail, jc.encode(detailObj))
+  const subScanReq = nc.subscribe("scan-request", { queue: `room-${SITE_NAME}` });
+  (async () => {
+    for await (const m of subScanReq) {
+      const urlObj = jc.decode(m.data);
+      console.log(urlObj)
+      const url = urlObj.url;
+      const siteName = urlObj.siteName;
+
+      if( siteName !== SITE_NAME) {
+        console.log(`It's not a request for me: [${siteName}]`)
+        continue;
+      }
+
+      console.log(`[${subScanReq.getProcessed()}]: ${siteName}`);
+
+      const roomDetail = await scanRoomDetail(url);
+      console.log(roomDetail)
+      nc.publish("scan-response", jc.encode({ roomDetail }));
+
     }
     console.log("subscription closed");
   })();
-})();
+})()
